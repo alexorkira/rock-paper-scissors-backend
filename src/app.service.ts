5e420ea1558ebdf471629e630585a77fa01f560c
Alessandro DESSI <alessandro.dessi@alten.com>
2021-06-19 10:09:20 +0200

    determine who wins

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,71 @@
 import { Injectable } from "@nestjs/common";
 import WeaponEnum from "./weapon/weapon.enum";
 import Weapon from "./weapon/weapon";
+import { WeaponFactory, WeaponSuite, WeaponSuiteFactory } from "./weapon/weapon.factory";
+import GameModeInputDto from "./dto/input/game-mode.input.dto";
+import MatchResultDto from "./dto/output/match-result.dto";
+import PlayerMoveDto from "./dto/input/player-move.input.dto";
 
 @Injectable()
 export class AppService {
-  private weapons: Weapon[];
+  private readonly weapons: WeaponSuite;
+  private mode: string;
+
+  private computerChoice;
 
   constructor() {
-    this.weapons = [
-      { id: WeaponEnum.ROCK, strongerThan: [WeaponEnum.SCISSORS] },
-      { id: WeaponEnum.PAPER, strongerThan: [WeaponEnum.ROCK] },
-      { id: WeaponEnum.SCISSORS, strongerThan: [WeaponEnum.PAPER] },
-    ];
+    this.weapons = WeaponSuiteFactory.createWeaponSuite();
   }
 
   /**
    * Returns a random choice between the all Weapons set up
    */
   getRandomWeapon(): WeaponEnum {
-    return Math.floor(Math.random() * this.weapons.length);
+    const allWeapons = Object.values(this.weapons);
+    const randomWeapon = allWeapons[Math.floor(Math.random() * allWeapons.length)];
+    return randomWeapon.name;
+  }
+
+  /**
+   * Set the mode of the match. P1 vs COM || COM vs COM
+   * @param dto: input dto containing the game mode
+   */
+  setGameMode(dto: GameModeInputDto): string {
+    this.mode = dto.mode;
+
+    // // If the user select Player VS Computer
+    // if (this.mode === "1player") {
+    //   this.computerChoice = this.getRandomWeapon();
+    // }
+
+    return "ok";
+  }
+
+  /**
+   * Discover who is the winner of the match
+   * @param dto: input dto containing the player move
+   */
+  discoverTheWinner(dto: PlayerMoveDto): MatchResultDto {
+    const { playerMove } = dto;
+
+    const computerMove = this.getRandomWeapon();
+
+    if (playerMove === computerMove) {
+      return { playerMove, computerMove };
+    }
+
+    if (this.weapons[playerMove].weaknesses.find((weaponName) => weaponName == computerMove)) {
+      return {
+        playerMove,
+        computerMove,
+        winner: "computer",
+      };
+    }
+
+    return {
+      playerMove,
+      computerMove,
+      winner: "you!!",
+    };
   }
 }
