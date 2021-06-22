f84e91b45d19881cef6aab579e6728505ce1cda7
Alessandro DESSI <alessandro.dessi@alten.com>
2021-06-22 12:19:48 +0200

    remove unused dto

--- src/dto/input/game-mode.input.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from "class-validator";
-
-class GameModeInputDto {
-  @IsNotEmpty()
-  mode: "1P" | "COM2";
-}
-
-export default GameModeInputDto;
--- src/dto/input/index.ts
+++ src/dto/input/index.ts
@@ -1,2 +1,1 @@
-export { default as GameModeDto } from "./game-mode.input.dto";
 export { default as PlayerChoiceDto } from "./player-choice.input.dto";
